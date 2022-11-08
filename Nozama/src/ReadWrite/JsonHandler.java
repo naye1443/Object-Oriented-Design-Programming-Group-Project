d90b4117df5e970f4e85d1588131554847872ed6
Jordan Diaz <30423497+JORDAN-DIAZ-ENG@users.noreply.github.com>
2022-11-08 13:08:09 -0500

    Add -methods for writing to Json -some comments on previous methods

--- Nozama/src/ReadWrite/JsonHandler.java
+++ Nozama/src/ReadWrite/JsonHandler.java
@@ -2,16 +2,23 @@ package ReadWrite;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+
 public class JsonHandler
 {
     JSONParser jsonParser = new JSONParser();
 
+    /**
+     * This method reads a JSON file and returns a JSONArray representing the data from the file
+     * @return JSONArray representing JSON file
+     * @author Jordan Diaz
+     * */
     public JSONArray getJSONArrayFromJson(String filename)
     {
         try (FileReader reader = new FileReader(filename))
@@ -36,6 +43,68 @@ public class JsonHandler
         }
 
     }
+
+    /**
+     * This method writes a JSONArray into to a given Json file completely overriding what was previously written
+     * @author Jordan Diaz
+     * */
+    public void writeToJson(String filename, JSONArray data)
+    {
+        try (FileWriter file = new FileWriter(filename))
+        {
+
+            String stringData = data.toJSONString();
+
+            String tabCount = "";
+            for (int i = 0; i < data.toJSONString().length(); i++)
+            {
+
+                if (stringData.charAt(i) == '[')
+                {
+                    tabCount += "\t";
+                    file.append(stringData.charAt(i) + "\n" + tabCount);
+
+
+                }
+                else if (stringData.charAt(i) == ']')
+                {
+                    file.append("\n" + stringData.charAt(i));
+                    tabCount = tabCount.substring(0, tabCount.length() - 1);
+
+                }
+                else if (stringData.charAt(i) == '{')
+                {
+                    tabCount += "\t";
+                    file.append(stringData.charAt(i) + "\n" + tabCount);
+
+
+                }
+                else if (stringData.charAt(i) == '}')
+                {
+                    file.append("\n" + tabCount + stringData.charAt(i));
+                    tabCount = tabCount.substring(0, tabCount.length() - 1);
+                }
+                else if (stringData.charAt(i) == ',')
+                {
+                    file.append(stringData.charAt(i) + "\n" + tabCount);
+                }
+                else
+                {
+                    file.append(stringData.charAt(i));
+
+                }
+            }
+
+            //file.write(data.toJSONString());
+            file.flush();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
 
 }
--- Nozama/src/User.java
+++ Nozama/src/User.java
@@ -1,7 +1,13 @@
+import org.json.simple.JSONObject;
+
 public class User
 {
-    public String userID, username, password, accountType;
+    protected String userID, username, password, accountType;
 
+    /**
+     * Constructor
+     * @author Jordan Diaz
+     * */
     public User(String ID, String username, String password, String accountType)
     {
         this.userID = ID;
@@ -10,6 +16,41 @@ public class User
         this.accountType = accountType;
     }
 
+    /**
+     * This Method redefines the variables of a specific User object
+     * @author Jordan Diaz
+     * */
+    public void reconstruct(String ID, String username, String password, String accountType)
+    {
+        this.userID = ID;
+        this.username = username;
+        this.password = password;
+        this.accountType = accountType;
+    }
+
+    /**
+     * This Method converts the variables of a specific User object into a JSONObject
+     * @return JSONObject representation of User object
+     * @author Jordan Diaz
+     * */
+    public JSONObject toJSONObject()
+    {
+        JSONObject data = new JSONObject();
+        data.put("username", this.username);
+        data.put("password", this.password);
+        data.put("accountType", this.accountType);
+
+        JSONObject header = new JSONObject();
+        header.put(this.userID, data);
+
+        return header;
+    }
+
+    /**
+     * This Method converts the variables of a specific User object into a String
+     * @return String representation of User object
+     * @author Jordan Diaz
+     * */
     @Override
     public String toString() {
         return "User{" +
