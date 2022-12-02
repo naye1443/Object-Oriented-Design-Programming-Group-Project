3252ca967855309f12737b674597bfbc6b074ce3
Naye1443 <76511694+naye1443@users.noreply.github.com>
2022-12-01 20:21:01 -0500

    small changes

--- Nozama/src/NozamaGui/View.java
+++ Nozama/src/NozamaGui/View.java
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-package NozamaGui;
-
-//// This creates the main view port for the system
-  public class View {
-//
-//  String[] State = {"DataTypes.User", "Seller", "login"};
-//
-//  Renders different state of application
-//  public void render{
-//
-//  System.out.println("rendered" + state[i]);
-//}
-//
-//  state = login
-//  state = user
-//
-//  // instance of Model.NozamaSystem in here
-
-}
-=======
 package NozamaGui;
 
 import javax.swing.*;
@@ -39,4 +18,3 @@ public class View
 
 
 }
->>>>>>> c29471d2da657eed314c0896672467ce89fa9f4b
